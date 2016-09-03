c3298f0d2298cbbad92b10a704d96d308629b9ac
FezZHD <fezzdev@gmail.com>
2016-09-04 00:12:10 +0300

    adding string stack project

---
+++ EPAM_Task/StringStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("StringStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("StringStack")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("53de3b36-9c25-45fb-981c-0891d1fa12dd")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EPAM_Task/StringStack/Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringStack
+{
+    public class StringStack
+    {
+        private string[] _stringStack;
+
+        public StringStack()
+        {
+            _stringStack = new string[1];
+        }
+
+
+        public void Push(string newString)
+        {
+            Array.Resize(ref _stringStack, _stringStack.Length + 1);
+            _stringStack[_stringStack.Length - 1] = newString;
+        }
+
+
+        public string Pop()
+        {
+            string popedString = _stringStack[0];
+            for (int i = 1; i < _stringStack.Length; i++)
+            {
+                _stringStack[i - 1] = _stringStack[i];
+            }
+
+            Array.Resize(ref _stringStack, _stringStack.Length -1);
+            return popedString;
+        }
+
+
+
+        public string Peak()
+        {
+            return _stringStack[0];
+        }
+
+
+        public string[] ToArray()
+        {
+            return _stringStack;
+        }
+
+
+        public void Clear()
+        {
+            Array.Clear(_stringStack, 0, _stringStack.Length);
+        }
+
+
+        public override bool Equals(object comparableObject)
+        {
+            return _stringStack.Equals(comparableObject);
+        }
+    }
+}
